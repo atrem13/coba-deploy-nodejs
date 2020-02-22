5ae2dd6b9e5c5d4df473e30b34ef62491de3c84b
atrem13 <mertayoga17.my@gmail.com>
2020-02-22 18:19:52 +0800

    edit index.html

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
+
 const http = require('http');
-const server = http.createServer((req, res)=>{
-  
-  // parse url
-  const parsedUrl = url.parse(req.url, true);
-  
-  // get http method
-  const method = req.method.toLowerCase();
-  
-  // get path
-  const path = parsedUrl.pathname;
+const url = require('url');
 
+const server = http.createServer((req, res) => {
+    //parse url
+    const parsedUrl = url.parse(req.url, true);
 
-  res.end("hello world this is method ${method} and path ${path}");
+    // get the path
+    const path = parsedUrl.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+    // Get the query string as an object
+    const queryStringObject = parsedUrl.query;
+
+    // Get the HTTP method
+    const method = req.method.toLowerCase();
+
+    res.setHeader('Content-Type', 'text/html');
+    res.end('Request pathname: '+trimmedPath+' with HTTP method: '+method+' and this query string: '+ JSON.stringify(queryStringObject));
 });
-server.listen(3000, ()=> {
-  console.log("server running on port 3000");
+
+server.listen(3000, () => {
+    console.log('The server is up and running on port 3000');
 });
